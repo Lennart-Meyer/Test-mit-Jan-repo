ac308df8076bc154cd432e7b23ded5ee482eb0bb
Lennart-Meyer <lennart.meyer@student.jade-hs.de>
2020-09-30 11:33:50 +0200

    Initial Commit

---
+++ TestAPP mit Jan/TestAPP mit Jan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPP_mit_Jan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Jan noch motiviert?");
+
+            Console.ReadKey();
+        }
+    }
+}
